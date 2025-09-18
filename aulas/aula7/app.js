9bd7fea6c4b81bc0afac808763f93ff7fcc57fd4
Mariana Kanashiro <m.kanashiro@iesb.edu.br>
2025-09-18 10:49:28 -0300

    codigo da aula7

--- aulas/aula7/app.js
+++ aulas/aula7/app.js
@@ -13,4 +13,24 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 
+const tarefas = [];
+
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas);
+});
+
+app.post('/tarefas', (req, res) => {
+    const novaTarefa = {
+        id: tarefas.length + 1,
+        nome: req.body.nome,
+        concluida: false
+    }
+    tarefas.push(novaTarefa);
+    res.status(201).json(novaTarefa);
+})
+
+app.get('/tarefas/:id', (req, res) => {
+    res.json({});
+});
+
 module.exports = app;
